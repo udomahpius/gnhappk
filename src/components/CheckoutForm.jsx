c45b25854065e0923d27ec7824d98735989475a5
Celebrity Developer <essiensaviour.a@gmail.com>
2023-09-29 20:08:51 +0100

    Updated

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -6,6 +6,7 @@ import {
   useElements
 } from "@stripe/react-stripe-js";
 import { BigButton, Button } from ".";
+import PaymentService from "@/services/PaymentService";
 
 export default function CheckoutForm() {
   const stripe = useStripe();
@@ -56,24 +57,28 @@ export default function CheckoutForm() {
 
     setIsLoading(true);
 
-    const { error } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        // Make sure to change this to your payment completion page
-        return_url: "https://app.setly.ai/payment/success",
-      },
-    });
+    const subscription = await PaymentService.createSubscription();
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
-      setMessage(error.message);
-    } else {
-      setMessage("An unexpected error occurred.");
-    }
+    console.log(subscription);
+
+    // const { error } = await stripe.confirmPayment({
+    //   elements,
+    //   confirmParams: {
+    //     // Make sure to change this to your payment completion page
+    //     return_url: "https://app.setly.ai/payment/success",
+    //   },
+    // });
+
+    // // This point will only be reached if there is an immediate error when
+    // // confirming the payment. Otherwise, your customer will be redirected to
+    // // your `return_url`. For some payment methods like iDEAL, your customer will
+    // // be redirected to an intermediate site first to authorize the payment, then
+    // // redirected to the `return_url`.
+    // if (error.type === "card_error" || error.type === "validation_error") {
+    //   setMessage(error.message);
+    // } else {
+    //   setMessage("An unexpected error occurred.");
+    // }
 
     setIsLoading(false);
   };
