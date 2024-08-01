62694824b427e9241ab5afee3ab96e1559ff5de0
Celebrity Developer <essiensaviour.a@gmail.com>
2024-08-01 12:47:54 +0100

    Remove unwanted files

--- src/services copy/autoplug.js
+++
@@ -1,9 +0,0 @@
-export const DEFAULT_AUTO_PLUG_COMMENT = "default-auto-plug-comment";
-
-export function storeAutoPlugComment(comment) {
-  localStorage.setItem(DEFAULT_AUTO_PLUG_COMMENT, comment);
-}
-
-export function getAutoPlugComment() {
-  return localStorage.getItem(DEFAULT_AUTO_PLUG_COMMENT);
-}
