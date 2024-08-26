d813a1e92fce897e8e0283246dfdc777ffa82494
Celebrity Developer <essiensaviour.a@gmail.com>
2024-08-26 09:19:31 +0100

    Add custom spinner

--- src/components/GlobalLoader.jsx
+++ src/components/GlobalLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAxiosLoader } from "@/api/useAxiosLoader";
-import loader from "@/assets/loader.svg";
+import loader from "@/assets/spinner.svg";
 import Image from "next/image";
 
 const GlobalLoader = () => {
@@ -9,9 +9,8 @@ const GlobalLoader = () => {
     <>
     {
       loading &&
-      <div className="fixed inset-0 z-50 grid w-screen h-screen p-4 bg-slate-400 place-items-center sm:p-6 lg:p-8 opacity-30">
+      <div className="fixed inset-0 z-50 grid w-screen h-screen p-4 bg-slate-600 place-items-center sm:p-6 lg:p-8 opacity-30">
         <Image width={30} height={30} className="w-auto h-16" src={loader} alt="" />
-
       </div>
       }
     </>
