1912c09a77e854af4bad27dacc5860f092a0ae6a
Celebrity Developer <essiensaviour.a@gmail.com>
2024-09-03 11:36:50 +0100

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import logo from "@/assets/good-logo.png";
 import hero from "@/assets/hero-1.png";
 import featureOne from "@/assets/feature-1.png";
 import featureTwo from "@/assets/feature-2.png";
+import itek from "@/assets/itek-logo.jpeg";
+import illustration from "@/assets/illus.jpeg";
 
 import Link from "next/link";
 
@@ -24,11 +26,11 @@ function Landing() {
             </Link>
 
             <div className="flex items-center gap-1">
-                <Link href="/auth/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">About</Link>
-                <Link href="/auth/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">How It Works</Link>
-                <Link href="/auth/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Donation Pools</Link>
-                <Link href="/auth/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Testimonials</Link>
-                <Link href="/auth/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Contact</Link>
+                <Link href="#about" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">About</Link>
+                <Link href="#howitworks" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">How It Works</Link>
+                <Link href="#donation" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Donation Pools</Link>
+                <Link href="#testimonials" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Testimonials</Link>
+                {/* <Link href="/auth/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Contact</Link> */}
             </div>
 
             <div className="flex items-center lg:order-2">
@@ -45,15 +47,12 @@ function Landing() {
       <section className="bg-white dark:bg-gray-900">
         <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white uppercase">Good <br /> Neighborhood </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A platform to Connect with like-minded individuals and find financial solutions tailored to your needs.
-                The platform is built with Automatic Computer Algorithm.
-            </p>
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white uppercase">Empowering Communities, <br /> One Donation at a Time</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A product of ITEK SERVICES, connecting like-minded individuals for a better tomorrow</p>
             <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-              
-                    {/* <Link href="https://www.figma.com/community/file/1125744163617429490" className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        <svg className="w-4 h-4 mr-2" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 300" width="1667" height="2500"><style type="text/css">.st0{fill:#0acf83}.st1{fill:#a259ff}.st2{fill:#f24e1e}.st3{fill:#ff7262}.st4{fill:#1abcfe}</style><title>Figma.logo</title><desc>Created using Figma</desc><path id="path0_fill" className="st0" d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"/><path id="path1_fill" className="st1" d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"/><path id="path1_fill_1_" className="st2" d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"/><path id="path2_fill" className="st3" d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"/><path id="path3_fill" className="st4" d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"/></svg> Get Figma file
-                    </Link> */}
+                <Link href="https://www.figma.com/community/file/1125744163617429490" className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    Join the Movement
+                </Link>
             </div>
           </div>
             <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
@@ -62,17 +61,19 @@ function Landing() {
         </div>
     </section>
     {/* End block */}
-    
+
     {/* Start block */}
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900" id="about">
         <div className="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
             <div className="col-span-2 mb-8">
-                <p className="text-lg font-medium text-purple-600 dark:text-purple-500">HOW IT WORKS</p>
-                <h2 className="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">This is a 2 to 1 escrow system</h2>
-                <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">There&apos;s no central account for people to dumb their funds. It&apos;s a donate to earned system.
-                    They&apos;re different donation pools ranging from <strong className="text-black">N5000 - N100,000</strong>.</p>
+                <p className="text-lg font-medium text-purple-600 dark:text-purple-500">Our Story</p>
+                <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Good Neighborhoods is a product of ITEK SERVICES, a business dedicated to harnessing technology to improve lives.</p>
+                <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Our platform connects individuals and communities, providing tailored financial solutions through a secure and efficient peer-to-peer donation system.</p>
             </div>
-            <div className="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
+            <div className="col-span-2 ">
+                <Image src={itek} alt="" />
+            </div>
+            {/* <div className="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
                 <div>
                     <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z" clipRule="evenodd"></path></svg>
                     <h3 className="mb-2 text-2xl font-bold dark:text-white">Multiple Pools</h3>
@@ -99,12 +100,59 @@ function Landing() {
                     <h3 className="mb-2 text-2xl font-bold dark:text-white">Digital Assets</h3>
                     <p className="font-light text-gray-500 dark:text-gray-400">We will later create a digital assets payment gateway to enable users contribute and earn with USDT too.</p>
                 </div>
+            </div> */}
+        </div>
+      </section>
+    {/* End block */}
+    
+    {/* Start block */}
+    <section className="bg-white dark:bg-gray-900" id="howitworks">
+        <div className="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
+            {/* <div className="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
+                <div>
+                    <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z" clipRule="evenodd"></path></svg>
+                    <h3 className="mb-2 text-2xl font-bold dark:text-white">Multiple Pools</h3>
+                    <p className="font-light text-gray-500 dark:text-gray-400">User choose a pool and donate, and earned 40% of his/her donation within 24-72 hours based on his/her pool inputs.</p>
+                </div>
+                <div>
+                    <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                    <h3 className="mb-2 text-2xl font-bold dark:text-white">Reward System</h3>
+                    <p className="font-light text-gray-500 dark:text-gray-400">10% referral rewards is scale àt the end of every qualified payment or earnings.</p>
+                </div>
+                <div>
+                    <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd"></path></svg>
+                    <h3 className="mb-2 text-2xl font-bold dark:text-white">Support System</h3>
+                    <p className="font-light text-gray-500 dark:text-gray-400">Members will be in physical places to coordinate each other to take a better advantage of the program.</p>
+                </div>
+                <div>
+                    <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path></svg>
+                    <h3 className="mb-2 text-2xl font-bold dark:text-white">Tranings</h3>
+                    <p className="font-light text-gray-500 dark:text-gray-400">They&apos;ll be training centers for digital economy and solutions with leaders in different states.</p>
+                </div>
+
+                <div>
+                    <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                    <h3 className="mb-2 text-2xl font-bold dark:text-white">Digital Assets</h3>
+                    <p className="font-light text-gray-500 dark:text-gray-400">We will later create a digital assets payment gateway to enable users contribute and earn with USDT too.</p>
+                </div>
+            </div> */}
+
+            <div className="col-span-2 ">
+                <Image src={illustration} alt="" />
+            </div>
+
+            <div className="col-span-2 mb-8">
+                <p className="text-lg font-medium text-purple-600 dark:text-purple-500">HOW IT WORKS</p>
+                <h2 className="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">Simple. Secure. Empowering</h2>
+                <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Good Neighborhoods utilizes an automatic computer algorithm to facilitate a secure and efficient peer-to-peer donation system. Our platform is not an investment platform, but rather an innovative solution for immediate financial needs.                </p>
             </div>
         </div>
       </section>
-    {/* End block
-    Start block */}
-    <section className="bg-gray-50 dark:bg-gray-800 pt-5">
+    {/* End block */}
+
+
+    {/* Start block */}
+    <section className="bg-gray-50 dark:bg-gray-800 pt-5" id="testimonials">
         <h2 className="text-center text-2xl font-bold">What Our Users Say</h2>
         <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-24 lg:px-6">
             <figure className="max-w-screen-md mx-auto">
@@ -126,11 +174,11 @@ function Landing() {
       </section>
     {/* End block
     Start block */}
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900" id="donation">
         <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
             <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
-                <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Designed people looking for an extra cash like you</h2>
-                <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Good Neighborhood we focus on technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Choose Your Donation Pool </h2>
+                <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Select from various donation pools, each with its own earning potential. Our platform features donation pools ranging from N5,000 to N100,000.</p>
             </div>
             <div className="space-y-8 lg:items-center lg:justify-center lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
                 {/* Pricing Card */}
